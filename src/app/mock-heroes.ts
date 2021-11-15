573374278bf731d912d8db97b2608ead4a097feb
Minaac-ops <72027505+Minaac-ops@users.noreply.github.com>
2021-11-15 20:53:56 +0100

    tour of heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Trini'},
+  {id: 12, name: 'Dr. Nice'},
+  {id: 13, name: 'Narco'},
+  {id: 14, name: 'RubberMan'},
+  {id: 15, name: 'Bombasto'},
+  {id: 16, name: 'Celeritas'},
+  {id: 17, name: 'Magneta'},
+  {id: 18, name: 'Dynama'},
+  {id: 19, name: 'Dr. IQ'},
+  {id: 20, name: 'Tornado'}
+]
